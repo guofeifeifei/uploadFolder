43f8eea3f182dafe219087920d61c9754a21162c
100 飞鱼2 <feiyu@189.local>
2018-04-16 15:02:08 +0800

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 飞鱼2100 on 2018/4/16.
+//  Copyright © 2018年 feiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by 飞鱼2100 on 2018/4/16.
+//  Copyright © 2018年 feiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
