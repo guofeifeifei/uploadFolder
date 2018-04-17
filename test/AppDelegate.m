9676dff6c3bfae0474803ed81ec91d385991da7a
gf <2545706530@qq.com>
2018-04-17 10:03:39 +0800

    提交

--- test/AppDelegate.m
+++ test/AppDelegate.m
@@ -20,7 +20,40 @@
     return YES;
 }
 
-
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    if (self.window) {
+        if (url) {
+            NSString *fileName = url.lastPathComponent; // 从路径中获得完整的文件名（带后缀）
+            // path 类似这种格式：file:///private/var/mobile/Containers/Data/Application/83643509-E90E-40A6-92EA-47A44B40CBBF/Documents/Inbox/jfkdfj123a.pdf
+            NSString *path = url.absoluteString; // 完整的url字符串
+            path = [self URLDecodedString:path]; // 解决url编码问题
+            
+            NSMutableString *string = [[NSMutableString alloc] initWithString:path];
+            
+            if ([path hasPrefix:@"file://"]) { // 通过前缀来判断是文件
+                // 去除前缀：/private/var/mobile/Containers/Data/Application/83643509-E90E-40A6-92EA-47A44B40CBBF/Documents/Inbox/jfkdfj123a.pdf
+                [string replaceOccurrencesOfString:@"file://" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, path.length)];
+                
+                // 此时获取到文件存储在本地的路径，就可以在自己需要使用的页面使用了
+                NSDictionary *dict = @{@"fileName":fileName,
+                                       @"filePath":string};
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"FileNotification" object:nil userInfo:dict];
+                
+                return YES;
+            }
+        }
+    }
+    return YES;
+}
+    
+    // 当文件名为中文时，解决url编码问题
+- (NSString *)URLDecodedString:(NSString *)str {
+    NSString *decodedString=(__bridge_transfer NSString *)CFURLCreateStringByReplacingPercentEscapesUsingEncoding(NULL, (__bridge CFStringRef)str, CFSTR(""), CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
+    NSLog(@"decodedString = %@",decodedString);
+    return decodedString;
+}
+    
+   
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- test/ViewController.m
+++ test/ViewController.m
@@ -9,16 +9,28 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+    @property (weak, nonatomic) IBOutlet UILabel *alertAlert;
+    
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fileNotification:) name:@"FileNotification" object:nil];
+    
 }
-
+- (void)fileNotification:(NSNotification *)notifcation {
+    NSDictionary *info = notifcation.userInfo;
+    // fileName是文件名称、filePath是文件存储在本地的路径
+    // jfkdfj123a.pdf
+    NSString *fileName = [info objectForKey:@"fileName"];
+    // /private/var/mobile/Containers/Data/Application/83643509-E90E-40A6-92EA-47A44B40CBBF/Documents/Inbox/jfkdfj123a.pdf
+    NSString *filePath = [info objectForKey:@"filePath"];
+    
+    NSLog(@"fileName=%@---filePath=%@", fileName, filePath);
+    self.alertAlert.text = [NSString stringWithFormat:@"正在上传文件，文件地址%@", filePath ];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
